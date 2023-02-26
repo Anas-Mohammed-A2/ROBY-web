6c392172c54a2727acd3d92a9d0c5a2e9ff2ef0a
Anas-Mohammed-A2 <anasmohamedgof@gmail.com>
2023-02-26 14:49:56 +0200

    add web

---
+++ main.js
@@ -0,0 +1,28 @@
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+
+  document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
+  document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+    return false;
+    }
+    }
